3ae6290799e7d97bbf0d0cf2a7f70476b39243e5
DilightDi <106067996+DilightDi@users.noreply.github.com>
2022-08-12 18:55:21 +0300

    Added file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string [] stringArray1 = new string[4] {"Kazan", "Ufa", "Moscow", "253"};
+string [] stringArray2 = new string[stringArray1.Length];
+int count = 0;
+
+for(int i = 0; i < stringArray1.Length; i++){
+    if(stringArray1[i].Length <= 3){
+        stringArray2[count] = stringArray1[i];
+        count++;
+    }
+}
+for(int i = 0; i < stringArray1.Length; i++){
+    Console.Write($"{stringArray2[i]} ");
+}
